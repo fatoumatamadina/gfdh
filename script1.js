d20b50e02fddd7b03130ff3de928eb8a7f5d46fc
fatoumatamadina <diawarafatoumatamadina859@gmail.com>
2026-05-06 14:07:34 +0200

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,24 @@
+// declaration d'une variable
+ 
+
+let nom = "fof"
+
+const prenom = "fode"
+
+let age = 25
+
+// affichage d'une variable
+
+// document.write(nom + " " + prenom + "age"  + age)
+let val1 = 6
+
+let val2 = 7
+
+document.write(val1 + "+" + val2 +"="+ (val1+val2)+"<br>")
+
+document.write(val1 + "-" + val2 +"="+ (val1-val2)+"<br>")
+
+document.write(val1 + "*" +val2 +"="+ (val1*val2)+"<br>")
+
+document.write(val1 + "%" + val2 +"="+ (val1%val2)+"<br>")
+
